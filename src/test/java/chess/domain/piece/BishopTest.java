a567d232c1ed2e5b9037522ecc4e2e4c9a649137
gyungchan Jo <namejgc@naver.com>
2024-03-28 13:20:19 +0900

    test: Bishop이 이동 가능한 위치를 판별하는 테스트 추가 및 픽스쳐 적용

--- src/test/java/chess/domain/piece/BishopTest.java
+++ src/test/java/chess/domain/piece/BishopTest.java
@@ -1,21 +1,20 @@
 package chess.domain.piece;
 
-import chess.domain.point.Point;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static chess.fixture.PointFixture.포인트;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BishopTest {
+    private Bishop bishop = new Bishop(Team.WHITE);
 
     @Test
     @DisplayName("오른쪽 대각선 위로 움직일 수 있다.")
     void isMovableDirection1() {
-        Bishop bishop = new Bishop(Team.WHITE);
         boolean result = bishop.isMovableDirection(
-                Point.of('a', 1),
-                Point.of('b', 2)
-        );
+                포인트("A1"),
+                포인트("B2"));
 
         assertThat(result).isTrue();
     }
@@ -23,11 +22,9 @@ class BishopTest {
     @Test
     @DisplayName("오른쪽 대각선 아래로 움직일 수 있다.")
     void isMovableDirection2() {
-        Bishop bishop = new Bishop(Team.WHITE);
         boolean result = bishop.isMovableDirection(
-                Point.of('c', 4),
-                Point.of('e', 2)
-        );
+                포인트("C4"),
+                포인트("E2"));
 
         assertThat(result).isTrue();
     }
@@ -35,11 +32,9 @@ class BishopTest {
     @Test
     @DisplayName("왼쪽 대각선 위로 움직일 수 있다.")
     void isMovableDirection3() {
-        Bishop bishop = new Bishop(Team.WHITE);
         boolean result = bishop.isMovableDirection(
-                Point.of('h', 1),
-                Point.of('a', 8)
-        );
+                포인트("H1"),
+                포인트("A8"));
 
         assertThat(result).isTrue();
     }
@@ -47,12 +42,50 @@ class BishopTest {
     @Test
     @DisplayName("왼쪽 대각선 아래로 움직일 수 있다.")
     void isMovableDirection4() {
-        Bishop bishop = new Bishop(Team.WHITE);
         boolean result = bishop.isMovableDirection(
-                Point.of('h', 8),
-                Point.of('b', 2)
-        );
+                포인트("H8"),
+                포인트("B2"));
 
         assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("오른쪽 일직선으로 움직일 수 없다.")
+    void isNotMovableDirection1() {
+        boolean result = bishop.isMovableDirection(
+                포인트("A1"),
+                포인트("H1"));
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("왼쪽 일직선으로 움직일 수 없다.")
+    void isNotMovableDirection2() {
+        boolean result = bishop.isMovableDirection(
+                포인트("C4"),
+                포인트("A4"));
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("위 일직선으로 움직일 수 없다.")
+    void isNotMovableDirection3() {
+        boolean result = bishop.isMovableDirection(
+                포인트("A1"),
+                포인트("A8"));
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("아래 일직선으로 움직일 수 없다.")
+    void isNotMovableDirection4() {
+        boolean result = bishop.isMovableDirection(
+                포인트("H8"),
+                포인트("B8"));
+
+        assertThat(result).isFalse();
     }
 }
