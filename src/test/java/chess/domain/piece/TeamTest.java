7f740d2f0dd668c30f0eb7d25d7905b143ac9737
gyungchan Jo <namejgc@naver.com>
2024-04-01 15:03:48 +0900

    test: 폰의 팀 별 이동 방향을 가져오는 기능 테스트

--- src/test/java/chess/domain/piece/TeamTest.java
+++ src/test/java/chess/domain/piece/TeamTest.java
@@ -9,6 +9,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class TeamTest {
 
     @ParameterizedTest
+    @CsvSource(value = {"WHITE, 1", "BLACK, -1"})
+    @DisplayName("폰의 팀 별 이동 방향을 알 수 있다.")
+    void forwardDirection(Team team, int expected) {
+        int actual = team.forwardDirection();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
     @CsvSource(value = {"WHITE, BLACK", "BLACK, WHITE"})
     @DisplayName("상대팀의 정보를 알 수 있다.")
     void opposite(Team currentTeam, Team expected) {
